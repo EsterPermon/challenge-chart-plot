edca4627784a6ec6e3499e171c570759fd66fd7e
EsterPermon <esterpmonteiro@gmail.com>
2020-07-19 17:20:12 -0300

    Creating the UserInputComponent
    This component renders the text area where the chart data will be
    inputted

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Toolbar from './components/Toolbar/Toolbar'
 import './App.css';
 import Footer from './components/Footer/Footer';
+import UserInput from './components/UserInput/UserInput';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Toolbar title={title}/>
+      <UserInput />
       <Footer/>
     </div>
   );
---
+++ src/components/UserInput/UserInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './UserInput.css'
+
+const UserInput = props => {
+
+  return(
+    <div className="user-input">
+      <textarea className="text-area"></textarea>
+    </div>
+  )
+}
+
+export default UserInput;
