4b2fdb63db59cde4ef925560f272071abd749644
EsterPermon <esterpmonteiro@gmail.com>
2020-07-22 18:50:28 -0300

    Replacing textarea by react-simple-code-editor

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,16 +1,22 @@
 import React, { useState, useRef, useEffect } from 'react';
 
+
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import '../../assets/prism-vsc-dark-plus.css';
+
 import './UserInput.css'
 
 const UserInput = React.memo(props => {
-
   const [enteredInput, setEnteredInput] = useState('');
-  const inputRef = useRef();
+  const editorRef = useRef();
   const {sendInput} = props;
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      if(enteredInput === inputRef.current.value){
+      if(enteredInput === editorRef.current.props.value){
         sendInput(enteredInput);
       }
     }, 1000);
@@ -19,12 +25,23 @@ const UserInput = React.memo(props => {
     }
   }, [enteredInput, sendInput]);
 
+  const hightlightWithLineNumbers = (input, language) =>
+  highlight(input, language)
+    .split("\n")
+    .map((line, i) => `<span class='editorLineNumber'>${i + 1}.</span>${line}`)
+    .join("\n");
+
   return(
-    <textarea
-      className="text-area"
-      onChange={event => setEnteredInput(event.target.value)} 
-      ref={inputRef}>
-    </textarea>
+    <div className="editor-container">
+      <Editor
+        className="editor"
+        value={enteredInput}
+        highlight={code => hightlightWithLineNumbers(code, languages.js)}
+        textareaId="textArea"
+        onValueChange={code => setEnteredInput(code)}
+        ref={editorRef}
+      />
+    </div>
   )
 });
 
